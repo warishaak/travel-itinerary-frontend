0efca7bc4716deefa56016ec3306a7df4eb0a958
warisha-k <warisha.kaleem@booking.com>
2026-03-27 15:40:36 +0000

    Add test coverage

--- vite.config.js
+++ vite.config.js
@@ -8,5 +8,23 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/__tests__/setup.js',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      exclude: [
+        'node_modules/**',
+        'src/__tests__/**',
+        '**/*.config.js',
+        '**/*.config.ts',
+        '**/dist/**',
+        '**/coverage/**',
+        '**/.{idea,git,cache,output,temp}/**',
+      ],
+      all: true,
+      lines: 80,
+      functions: 80,
+      branches: 80,
+      statements: 80,
+    },
   },
 })
